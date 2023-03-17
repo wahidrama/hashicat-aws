6c7162b5807d6a76d0a8ef47ab5cf51c61dfb3ff
Wahid Ramadhan <wahidramadhan791@gmail.com>
2023-03-17 12:46:55 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
   
-  bucket = "my-s3-bucket"
+  bucket = "wahidrama-s3-bucket"
   acl    = "private"
 
   versioning = {
